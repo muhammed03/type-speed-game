f7e101eca1bf4dc116ee48d4c2e153f5bdaefcaf
Muhammed Abdrahman <muhammed03amin@gmail.com>
2021-04-25 15:37:37 +0600

    feat: implement results container

--- script.js
+++ script.js
@@ -4,16 +4,26 @@ const scoreEl = document.getElementById("score");
 const timeEl = document.getElementById("time");
 const endgameEl = document.getElementById("end-game-container");
 const settingsBtn = document.getElementById("settings-btn");
+const resultsBtn = document.getElementById("results-btn");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
 const difficultySelect = document.getElementById("difficulty");
-
-getWords();
+const easyColumn = document.getElementById("easy");
+const hardColumn = document.getElementById("hard");
+const mediumColumn = document.getElementById("medium");
+const closeBtn = document.getElementById("close-btn");
 
 let words = [];
 
-// Focus on input
 text.focus();
+let scoreResults =
+  localStorage.getItem("scoreResults") !== null
+    ? JSON.parse(localStorage.getItem("scoreResults"))
+    : [];
+
+let index =
+  localStorage.getItem("index") !== null ? localStorage.getItem("index") : 0;
+
 // async function to fetch random words
 async function getWords() {
   const res = await fetch(
@@ -54,6 +64,17 @@ function gameRound() {
     return words[Math.floor(Math.random() * words.length)];
   }
 
+  // Add current result to the array
+  function addScoreToResultsData() {
+    const scoreData = {
+      id: index,
+      scoreMode: difficulty,
+      result: score,
+    };
+    scoreResults.push(scoreData);
+    index++;
+  }
+
   function addWordToDom() {
     randomWord = getRandomWord();
     word.innerHTML = randomWord;
@@ -82,9 +103,12 @@ function gameRound() {
     endgameEl.innerHTML = `
         <h1>Time ran out</h1>
         <p>Your final score is ${score}</p>
-        <button onclick="location.reload()">Reload</button>
+        <button onclick="updateRound()">Reload</button>
       `;
     endgameEl.style.display = "flex";
+    scoreEl.innerHTML = 0;
+    timeEl.innerHTML = 10 + "s";
+    addScoreToResultsData();
   }
 
   addWordToDom();
@@ -122,3 +146,79 @@ function gameRound() {
     localStorage.setItem("difficulty", difficulty);
   });
 }
+
+function updateRound() {
+  endgameEl.style.display = "none";
+  text.value = "";
+  gameRound();
+  updateLocalStorage();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("scoreResults", JSON.stringify(scoreResults));
+  localStorage.setItem("index", index);
+}
+
+// Add results to container
+function addResults() {
+  scoreResults
+    .filter(function (item) {
+      return item.scoreMode === "hard";
+    })
+    .sort(function (a, b) {
+      return b.result - a.result;
+    })
+    .map(function (e) {
+      let hardItem = document.createElement("div");
+      hardItem.innerText = e.result;
+
+      if (e.result > 0) {
+        hardColumn.appendChild(hardItem);
+      }
+    });
+
+  scoreResults
+    .filter(function (item) {
+      return item.scoreMode === "easy";
+    })
+    .sort(function (a, b) {
+      return b.result - a.result;
+    })
+    .map(function (e) {
+      let easyItem = document.createElement("div");
+      easyItem.innerText = e.result;
+      if (e.result > 0) {
+        easyColumn.appendChild(easyItem);
+      }
+    });
+
+  scoreResults
+    .filter(function (item) {
+      return item.scoreMode === "medium";
+    })
+    .sort(function (a, b) {
+      return b.result - a.result;
+    })
+    .map(function (e) {
+      let item = document.createElement("div");
+      item.innerText = e.result;
+      if (e.result > 0) {
+        mediumColumn.appendChild(item);
+      }
+    });
+}
+
+function showResults() {
+  document.querySelector(".popup-container").style.display = "flex";
+  addResults();
+}
+
+function closeResults() {
+  document.querySelector(".popup-container").style.display = "none";
+}
+
+// Show results
+resultsBtn.addEventListener("click", showResults);
+
+// Clsoe results
+closeBtn.addEventListener("click", closeResults);
